961f0acce80330f0dffda700df8d86237ca63c69
Sergio Díaz <codediaz@outlook.es>
2023-10-14 12:57:02 -0500

    feature: Fix pipeline

--- src/components/Navbar/Navbar/index.js
+++ src/components/Navbar/Navbar/index.js
@@ -9,7 +9,7 @@ export default function Navbar() {
         <div className="container">
           <div>
             <Bookmark/>
-            <h1>To Do</h1>
+            <h1>Grupo3</h1>
           </div>
           
         </div>
